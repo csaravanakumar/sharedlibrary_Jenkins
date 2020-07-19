956e522163e91ff33201b0976e8c085bd3e3ffbb
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-07-19 10:53:43 -0700

    Update oddoreven.groovy

--- vars/oddoreven.groovy
+++ vars/oddoreven.groovy
@@ -1,25 +1,5 @@
-def call(String name = 'human') {
-  if (name=='human') {
-    pipeline {
-      agent any
-      stages {
-        stage('Even Stage') {
-          steps {
-            echo "The build number is even"
-          }
-        }
-      }
-    }
-  } else {
-    pipeline {
-      agent any
-      stages {
-        stage('Odd Stage') {
-          steps {
-            echo "The build number is odd"
-          }
-        }
-      }
-    }
-  }
+import simple.sample
+
+def call()
+{
 }
