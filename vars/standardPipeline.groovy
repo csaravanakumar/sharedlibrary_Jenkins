24b7aeee5a66772be18be81030aabca387fac434
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-07-19 10:15:38 -0700

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -1,25 +1,5 @@
-def call() {
-  if (10 % 2 == 0) {
-    pipeline {
-      agent any
-      stages {
-        stage('Even Stage') {
-          steps {
-            echo "The build number is even"
-          }
-        }
-      }
-    }
-  } else {
-    pipeline {
-      agent any
-      stages {
-        stage('Odd Stage') {
-          steps {
-            echo "The build number is odd"
-          }
-        }
-      }
-    }
-  }
+#!/usr/bin/env groovy
+
+def call(String name = 'human') {
+    echo "Hello, ${name}."
 }
