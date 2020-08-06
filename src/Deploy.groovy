f98b135aed79eadd31d5036bd518748c7b945827
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-06 11:53:15 -0700

    Update Deploy.groovy

--- src/Deploy.groovy
+++ src/Deploy.groovy
@@ -1,7 +1,16 @@
-class Deployer
-{
-def run()
-{
-echo "deployment success"
-}
+  
+@Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.6')
+import org.apache.commons.lang3.StringUtils
+
+class Deployer {
+    int tries = 0
+    Script script
+
+    def run() {
+        while (tries < 10) {
+            Thread.sleep(1000)
+            tries++
+            script.echo("tries is numeric: " + StringUtils.isAlphanumeric("" + tries))
+        }
+    }
 }
