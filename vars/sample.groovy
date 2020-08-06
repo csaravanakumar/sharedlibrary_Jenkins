ac1806f4282414f414c3264a06134a56fcc40497
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-06 10:12:37 -0700

    Update sample.groovy

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -3,10 +3,5 @@
 def call(body) {
     echo "Check status"
 
-    (1..3).each {
-        echo "Number: " + it
-    }
-
-    currentBuild.result = 'SUCCESS' //FAILURE to fail
-    return this
+      return this
 }
