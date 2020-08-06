385cb70926bcfb089d3449285e8229b594a0f913
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-06 11:23:27 -0700

    Update Sample.groovy

--- src/simple/Sample.groovy
+++ src/simple/Sample.groovy
@@ -1,17 +1,5 @@
 public class Sample {
-    private int x;
-    private int y;
-
-    public Sample(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public String toString() {
-        return "Sample{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+    def P(){
+        echo "src test"
     }
 }
