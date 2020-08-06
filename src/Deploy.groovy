1754c8a698ea774686c5d29b3e1e1084ba0e3e54
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-06 11:55:48 -0700

    Update Deploy.groovy

--- src/Deploy.groovy
+++ src/Deploy.groovy
@@ -2,7 +2,7 @@
 @Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.6')
 import org.apache.commons.lang3.StringUtils
 
-class Deployer {
+class Deploy {
     int tries = 0
     Script script
 
