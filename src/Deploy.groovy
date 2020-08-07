51716e6ec4451f1756c2d30a0a2c0c1272817ab4
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-07 06:49:20 -0700

    Update Deploy.groovy

--- src/Deploy.groovy
+++ src/Deploy.groovy
@@ -1,16 +1,1 @@
   
-@Grab(group = 'org.apache.commons', module = 'commons-lang3', version = '3.6')
-import org.apache.commons.lang3.StringUtils
-
-class Deploy {
-    int tries = 0
-    Script script
-
-    def run() {
-        while (tries < 10) {
-            Thread.sleep(1000)
-            tries++
-            script.echo("tries is numeric: " + StringUtils.isAlphanumeric("" + tries))
-        }
-    }
-}
