426c96c7fa6a81b4e9274cc83e2e28b75fc12d96
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-07 09:27:14 -0700

    Update environ.groovy

--- vars/environ.groovy
+++ vars/environ.groovy
@@ -2,4 +2,7 @@ def call(){
 def env = System.getenv()
 println(env['JENKINS_HOME'])
 println(env[BUILD_ID])
+  environment {
+        def BUILDVERSION = sh(script: "echo `date +%s`", returnStdout: true).trim()
+    }
 }
