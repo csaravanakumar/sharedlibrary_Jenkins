6e2e5f4573aff7539b7ad2214590b5030fcb6036
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-07 06:53:03 -0700

    Update sample.groovy

--- src/org/saro/sample.groovy
+++ src/org/saro/sample.groovy
@@ -1,13 +1,1 @@
-package org.saro
-class sample implements Serializable {
-  def env
-  def steps
-  sample(env, steps) {
-    this.env = env
-    this.steps = steps
-  }
 
-  def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
-}
