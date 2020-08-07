2f830a39154b7c6828b863710d9010a8876ef77d
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-07 06:21:58 -0700

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -3,8 +3,7 @@
 def call(body) {
     echo "Start Deploy"
 
-   def d1=new Deploy1()
-    d1.run()
+  new Deploy1().run()
 
     echo "Deployed"
     currentBuild.result = 'SUCCESS' //FAILURE to fail
