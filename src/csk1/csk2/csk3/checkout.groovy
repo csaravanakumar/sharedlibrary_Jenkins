ef88706a5466c4a142b57bf61a34b9316bcffe37
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-07 06:52:12 -0700

    Update checkout.groovy

--- src/csk1/csk2/csk3/checkout.groovy
+++ src/csk1/csk2/csk3/checkout.groovy
@@ -1,18 +1,1 @@
-package csk1.csk2.csk3;
 
-import static Constants.GITHUB_CREDENTIALS_ID;
-
-def checkout(String repo, 
-    String branch = "master"){
-    stage("Checkout"){
-        checkout([
-            $class: 'GitSCM', 
-            branches: [[name: "${branch}"]], 
-            doGenerateSubmoduleConfigurations: false, 
-            userRemoteConfigs: [[
-                credentialsId: "${GITHUB_CREDENTIALS_ID}", 
-                url: "${repo}"
-            ]]
-        ])
-    }
-}
