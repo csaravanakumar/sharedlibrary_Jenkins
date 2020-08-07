394aba23535d9c8cb2fc6a8289f83d0f335fafd6
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-07 09:52:18 -0700

    Update envtime.groovy

--- vars/envtime.groovy
+++ vars/envtime.groovy
@@ -1,6 +1,9 @@
 def call(){
 pipeline {
     environment {
+        Date date = new Date()
+        String datePart = date.format("dd/MM/yyyy")
+        String timePart = date.format("HH:mm:ss")
     SOME_NAME = "dev1"
        }
    agent {label 'EC2StaticJenkinsSlaveFinal'}
@@ -8,7 +11,7 @@ pipeline {
     stages {
         stage("Env Variables") {
             steps {
-                echo "The build number is ${env.BUILD_NUMBER}"
+                echo "The build number is ${env.BUILD_NUMBER}->${datePart}"
                 echo "You can also use \${datePart} -> ${timePart}->${SOME_NAME}"
                 sh 'echo "I can access $BUILD_NUMBER in shell command as well."'
             }
