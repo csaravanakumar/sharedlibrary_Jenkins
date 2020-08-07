b2220e080fcc0a1d30b1c54e236aa366f4017b0c
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-07 08:34:54 -0700

    Update Sample.groovy

--- src/simple/Sample.groovy
+++ src/simple/Sample.groovy
@@ -8,10 +8,10 @@ class Sample implements Serializable {
 		this.script=script
 	}
 	def logHelloWorld() {
-		echo("hello world")
+		echo 'hello world'
 	}
 	def printHello() {
-	echo("hello world from library")
+	echo 'hello world from library'
 	}
 }
 
