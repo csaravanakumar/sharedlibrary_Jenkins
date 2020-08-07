d51c86dff9999adaba7033c06dcd5bc53b03b10b
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-07 08:08:00 -0700

    Update Sample.groovy

--- src/simple/Sample.groovy
+++ src/simple/Sample.groovy
@@ -1,12 +1,18 @@
 
-package simple
+package simple.csk
 
-class Sample1 {
+class Sample1 implements Serializable {
+	def script
+	Sample1(script)
+	{
+		this.script=script
+	}
 	def logHelloWorld() {
 		sh 'echo "hello world"'
 	}
+	def printHello() {
+	sh 'echo "hello world from library"'
+	}
 }
 
-def printHello() {
-	sh 'echo "hello world from library"'
-}
+
