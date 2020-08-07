7a990619f9213a660ffe10ca14e8d3f42867e9f8
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-07 06:50:04 -0700

    Update Deploy1.groovy

--- src/Deploy1.groovy
+++ src/Deploy1.groovy
@@ -1,13 +1,1 @@
-class Deploy1 implements Serializable
-{
-   Deploy1()
-   {
-   }
-   
-    def run() {
-        while (tries < 10) {
-             print "Hello World"
-             print "vrna"
-        }
-    }
-}
+
