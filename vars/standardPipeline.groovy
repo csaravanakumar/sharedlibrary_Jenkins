0b3d180648c4b4e66a6bdfdb99d5334e63acfacf
csaravanakumar <67650859+csaravanakumar@users.noreply.github.com>
2020-08-07 06:46:48 -0700

    Update standardPipeline.groovy

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -4,9 +4,7 @@ def util = fileLoader.load('Deploy1.groovy')
 def call(body) {
     echo "Start Deploy"
 
-  new Deploy1 new().run()
-
-    echo "Deployed"
+     echo "Deployed"
     currentBuild.result = 'SUCCESS' //FAILURE to fail
 
     return this
